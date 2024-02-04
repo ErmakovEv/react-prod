e84d276ba68ee3beb3222c611ad756ce0ccac872
ErmakovEv <ej-88@ya.ru>
2024-02-04 23:44:42 +0300

    sidebar stories withRouter fix import

--- src/widgets/SidebarBlock/ui/Sidebar/Sidebar.stories.tsx
+++ src/widgets/SidebarBlock/ui/Sidebar/Sidebar.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import ThemeDecorator from 'shared/config/DecoratorsStorybook/ThemeDecorator';
 import { Theme } from 'app/providers/theme';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { withRouter } from 'storybook-addon-react-router-v6';
 import { Sidebar } from './Sidebar';
 
